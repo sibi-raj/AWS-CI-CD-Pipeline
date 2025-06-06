628c3e0132d00519644acfc59ca511e8488ddb91
sibi-raj <sibiraj1817@gmail.com>
2025-06-06 18:57:32 +0530

    port fixing

--- application.py
+++ application.py
@@ -1,9 +1,9 @@
 from flask import Flask
-application = Flask(__name__) # This is the 'application' Gunicorn is looking for
+application = Flask(__name__)  # This is the 'application' Gunicorn looks for
 
 @application.route('/')
 def hello_world():
     return 'Hello, world!'
 
 if __name__ == '__main__':
-    application.run()
+    application.run(host='0.0.0.0', port=8000)
